307757655487f907c5169f74eec30b9214458fd8
Ana Daniel <anadanield@gmail.com>
2016-06-27 12:17:43 +0200

    add visualization for the walktrap c++ alg

---
+++ src/walktrap_visualization.py
@@ -0,0 +1,89 @@
+#! /usr/bin/python
+
+import matplotlib.pyplot as plt
+from graph_db import nodesAndEdges
+import numpy
+import networkx as nx
+import subprocess
+import random
+import pdb
+
+nodes, edges = nodesAndEdges()
+length = len(nodes)
+
+# Airports dictionary CODE: ID
+airports = {}
+
+# A saves the weight of the edges
+A = numpy.zeros((length,length))
+
+i = 0
+# Initiate dictionary for each airport
+for airport in nodes:
+  airports[airport.code] = i
+  i = i + 1
+
+# Create graph
+G = nx.Graph()
+
+for route in edges:
+  origin_index = airports[route.origin]
+  dest_index = airports[route.dest]
+  # Populate A with connections between airports
+  A[origin_index, dest_index] = route.freq
+
+# Create the file that will be the input for the walktrap algorithm
+f = open('flights.net','w')
+
+i = 0
+# Fill file and networkx simple graph
+while i < (length):
+  j = i + 1
+  while j < (length):
+    if A[i][j] > 0:
+      f.write("%d %d %d\n" % (i,j, A[i][j]))
+      G.add_edge(i, j, frequency=A[i][j])
+    j = j + 1
+  i = i + 1
+
+f.close()
+
+# run walktrap
+pipe = subprocess.Popen(["../walktrap02/walktrap", "flights.net", "-d1", "-s", "-b"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout_data, stderr_data = pipe.communicate()
+
+output_lines = stdout_data.splitlines()
+
+# Get the best modularity
+Q = float(output_lines[0])
+
+# Get the rest of the output
+communities = output_lines[1:(len(output_lines))]
+# Split the nodes in every line
+communities = [ nodes.split(" ") for nodes in communities ]
+# to int
+communities = map( lambda x: map( lambda y: int(y), x), communities)
+
+# pos = nx.spring_layout(G) # positions for all nodes
+pos = nx.shell_layout(G, communities)
+
+# nodes
+for c in communities:
+  color = "#%06x" % random.randint(0, 0xFFFFFF)
+  nx.draw_networkx_nodes(G,pos,
+                        nodelist= c,
+                        node_color=color,
+                        node_size=700)
+
+nx.draw_networkx_edges(G,pos,
+                       edgelist=G.edges(),
+                       width=1,
+                       alpha=0.5,
+                       edge_color='black')
+
+plt.axis('off')
+plt.show() # display
+
+
+
+
